47aa2720378d1de27f72a57c51eaab3f14da1025
Dipesh_Singh <84814627+dipeshsingh253@users.noreply.github.com>
2022-04-15 20:55:18 +0530

    week-1 day-1
    This consists problems from week-1 day-1 assignment java script variables

---
+++ Unit-1/Week-1/javascript variables/problem2.js
@@ -0,0 +1,14 @@
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name="Dipesh Singh";
+console.log(name);
+let father_name="Ramdayal Singh";
+console.log(father_name);
+let mother_name="Meera Singh";
+console.log(mother_name);
---
+++ Unit-1/Week-1/javascript variables/problem4.js
@@ -0,0 +1,24 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+let name="Dipesh Singh";
+let school="Masai School";
+let grade="A";
+let section="javascript";
+let rollno=21;
+let marks=91;
+console.log("**************************************************")
+console.log("REPORT CARD");
+console.log("**************************************************")
+
+console.log("Student Name:"+ " "+name);
+console.log("School Name:"+ " "+school);
+console.log("Grade:"+ " "+grade);
+console.log("Section:"+ " "+section);
+console.log("Roll No:"+ " "+rollno);
+console.log("Marks:"+ " "+marks);
+console.log("**************************************************")
+
+
